2472c63bd39c8b79741b728c5ea3044c4f746737
malcolm stanley <a.malcolm.stanley@gmail.com>
2021-04-25 20:10:15 -0400

    adding code written to date

---
+++ test_1_single_sensor/test_1_single_sensor.ino
@@ -0,0 +1,124 @@
+ /*
+ * Rui Santos
+ * Complete Project Details https://randomnerdtutorials.com
+ * Modified by Malcolm Stanley
+ * April 2021
+*/
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+// Data wire is plugged into port 1 on the Arduino
+#define ONE_WIRE_BUS 1
+
+// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature. 
+DallasTemperature sensors(&oneWire);
+
+int numberOfDevices; // Number of temperature devices found
+
+DeviceAddress tempDeviceAddress; // We'll use this variable to store a found device address
+
+void setup(void) {
+  // start serial port
+  Serial.begin(9600);
+  
+  // Start up the library
+  sensors.begin();
+
+// so this next bit is NOT going to work
+// when I was googling around it looked like what is happening is that 
+// this portion of the code does not execute properly for timing reasons
+// related to how some of the driver code is implemented. 
+
+  
+  // Grab a count of devices on the wire
+  numberOfDevices = sensors.getDeviceCount();
+  
+  // locate devices on the bus
+  Serial.print("Locating devices...");
+  Serial.print("Found ");
+  Serial.print(numberOfDevices, DEC);
+  Serial.println(" devices.");
+
+  // Loop through each device, print out address
+  for(int i=0;i<numberOfDevices; i++) {
+    // Search the wire for address
+    if(sensors.getAddress(tempDeviceAddress, i)) {
+      Serial.print("Found device ");
+      Serial.print(i, DEC);
+      Serial.print(" with address: ");
+      printAddress(tempDeviceAddress);
+      Serial.println();
+    } else {
+      Serial.print("Found ghost device at ");
+      Serial.print(i, DEC);
+      Serial.print(" but could not detect address. Check power and cabling");
+    }
+  }
+}
+
+void loop(void) { 
+
+// so if you see that this does not work above, 
+// take the SAME CODE BLOCK and put it in the loop et voila
+// it works here, in the loop, but not above, before the loop.
+// go figure.
+
+
+// Grab a count of devices on the wire
+  numberOfDevices = sensors.getDeviceCount();
+  
+  // locate devices on the bus
+  Serial.print("Locating devices...");
+  Serial.print("Found ");
+  Serial.print(numberOfDevices, DEC);
+  Serial.println(" devices.");
+
+  // Loop through each device, print out address
+  for(int i=0;i<numberOfDevices; i++) {
+    // Search the wire for address
+    if(sensors.getAddress(tempDeviceAddress, i)) {
+      Serial.print("Found device ");
+      Serial.print(i, DEC);
+      Serial.print(" with address: ");
+      printAddress(tempDeviceAddress);
+      Serial.println();
+    } else {
+      Serial.print("Found ghost device at ");
+      Serial.print(i, DEC);
+      Serial.print(" but could not detect address. Check power and cabling");
+    }
+  }
+  
+  sensors.requestTemperatures(); // Send the command to get temperatures
+  
+  // Loop through each device, print out temperature data
+  for(int i=0;i<numberOfDevices; i++) {
+    // Search the wire for address
+    if(sensors.getAddress(tempDeviceAddress, i)){
+    
+    // Output the device ID
+    Serial.print("Temperature for device: ");
+    Serial.println(i,DEC);
+
+    // Print the data
+    float tempC = sensors.getTempC(tempDeviceAddress);
+    Serial.print("Temp C: ");
+    Serial.print(tempC);
+    Serial.print(" Temp F: ");
+    Serial.println(DallasTemperature::toFahrenheit(tempC)); // Converts tempC to Fahrenheit
+    }   
+  }
+  delay(5000);
+}
+
+// function to print a device address
+void printAddress(DeviceAddress deviceAddress) {
+  for (uint8_t i = 0; i < 8; i++) {
+    if (deviceAddress[i] < 16) Serial.print("0");
+      Serial.print(deviceAddress[i], HEX);
+  }
+}
---
+++ test_2_multisensor_query_by_address/test_2_multisensor_query_by_address.ino
@@ -0,0 +1,160 @@
+/*
+ * Malcolm Stanley
+ * Pool Heater Temperature Sensor
+*/
+
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <WiFi101.h>
+
+#include "arduino_secrets.h"
+
+// Data wire is plugged into port 4 on the Arduino
+#define ONE_WIRE_BUS 2
+
+// Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Pass our oneWire reference to Dallas Temperature. 
+DallasTemperature sensors(&oneWire);
+
+// Labeled Temperature Sensors are as follows
+char intake[]   = "283AD1ED0C0000C0";
+char midpoint[] = "2811B0EC0C0000DD";
+char outlet[]   = "28315DEC0C00005D";
+
+DeviceAddress intakeAddress = { 0x28, 0x3A, 0xD1, 0xED, 0x0C, 0x00, 0x00, 0xC0 };
+DeviceAddress midpointAddress = { 0x28, 0x11, 0xB0, 0xEC, 0x0C, 0x00, 0x00, 0xDD };
+DeviceAddress outletAddress = { 0x28, 0x31, 0x5D, 0xEC, 0x0C, 0x00, 0x00, 0x5D };
+
+// these next items are in a password file I have gitignored. 
+// referenced here so you can see you need to declare them
+// tutorial for this is at https://www.andreagrandi.it/2020/12/16/how-to-safely-store-arduino-secrets/
+// note the include syntax above is pretty specific
+
+String apiKey ="SECRET_API_KEY_FROM_THINGSPEAK"; // api from ThingSpeak
+char ssid[] = "SECRET_SSID_FOR WIFI_AP"; //  your network SSID (name)
+char pass[] = "SECRET_WIFI_PASSWORD";    //your network password
+
+
+// next #define WEBSITE "api.thingspeak.com"
+int status = WL_IDLE_STATUS;
+// if you don't want to use DNS (and reduce your sketch size)
+// use the numeric IP instead of the name for the server:
+//IPAddress server(74,125,232,128);  // numeric IP for Google (no DNS)
+char server[] = "api.thingspeak.com";    // name address for Google (using DNS)
+
+// Initialize the Ethernet client library
+// with the IP address and port of the server
+// that you want to connect to (port 80 is default for HTTP):
+WiFiClient client;
+
+void setup() {
+  //Initialize serial and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) 
+  {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  // check for the presence of the shield:
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    // don't continue:
+    while (true);
+  }
+
+  // attempt to connect to Wifi network:
+  while (status != WL_CONNECTED) 
+  {
+    Serial.println("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    //Connect to WPA/WPA2 network.Change this line if using open/WEP network
+    status = WiFi.begin(ssid, pass);
+
+    // wait 10 seconds for connection:
+    delay(10000);
+  }
+  
+  Serial.println("Connected to wifi");
+  printWifiStatus();
+
+  // start serial port
+  Serial.begin(9600);
+  
+  // Start up the library
+  sensors.begin();
+}
+
+void loop(void) { 
+  
+  // Print the data
+  float tempIntake = sensors.getTempC(intakeAddress);
+    Serial.println("Intake Sensor ");
+    Serial.print(intake);
+    Serial.print(": Temp C: ");
+    Serial.print(tempIntake);
+    Serial.print(" Temp F: ");
+    Serial.println(DallasTemperature::toFahrenheit(tempIntake)); // Converts tempC to Fahrenheit
+  
+  float tempMidpoint = sensors.getTempC(midpointAddress);
+    Serial.println("Midpoint Sensor ");
+    Serial.print(midpoint);
+    Serial.print(": Temp C: ");
+    Serial.print(tempMidpoint);
+    Serial.print(" Temp F: ");
+    Serial.println(DallasTemperature::toFahrenheit(tempMidpoint)); // Converts tempC to Fahrenheit
+  
+  float tempOutlet = sensors.getTempC(outletAddress);
+    Serial.println("Outlet Sensor ");
+    Serial.print(outlet);
+    Serial.print(": Temp C: ");
+    Serial.print(tempOutlet);
+    Serial.print(" Temp F: ");
+    Serial.println(DallasTemperature::toFahrenheit(tempOutlet)); // Converts tempC to Fahrenheit
+
+// create data string to send to ThingSpeak
+  String data = String("field1=" + String(DallasTemperature::toFahrenheit(tempIntake), DEC) + "&field2=" + String(DallasTemperature::toFahrenheit(tempMidpoint), DEC) + "&field3=" + String(DallasTemperature::toFahrenheit(tempOutlet), DEC)); 
+  
+  // close any connection before sending a new request
+  client.stop();
+
+  // POST data to ThingSpeak
+  if (client.connect(server, 80)) {
+    client.println("POST /update HTTP/1.1");
+    client.println("Host: api.thingspeak.com");
+    client.println("Connection: close");
+    client.println("User-Agent: ArduinoWiFi/1.1");
+    client.println("X-THINGSPEAKAPIKEY: "+apiKey);
+    client.println("Content-Type: application/x-www-form-urlencoded");
+    client.print("Content-Length: ");
+    client.print(data.length());
+    client.print("\n\n");
+    client.print(data);
+    Serial.println("Data Sent to Thingspeak:");
+    Serial.print(data);
+    } else {
+      Serial.println("*** Connection to Thingspeak Not Established");
+    }
+  
+  delay(60000); 
+}
+
+void printWifiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+  
+  }   
+  
