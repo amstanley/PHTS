27c535a587c0b22e9eca7da6fc324474dc598c3c
malcolm stanley <a.malcolm.stanley@gmail.com>
2021-04-25 20:26:24 -0400

    small change

--- test_2_multisensor_query_by_address/test_2_multisensor_query_by_address.ino
+++ test_2_multisensor_query_by_address/test_2_multisensor_query_by_address.ino
@@ -9,7 +9,7 @@
 
 #include "arduino_secrets.h"
 
-// Data wire is plugged into port 4 on the Arduino
+// Data wire is plugged into port 2 on the Arduino
 #define ONE_WIRE_BUS 2
 
 // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
